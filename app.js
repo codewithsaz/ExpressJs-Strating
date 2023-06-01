bc332a48a10a4b108c2ff9e7af069fd4e24d5d9c
codewithsaz <codewithsaz@gmail.com>
2023-06-01 12:03:57 +0530

    ExpressJs-Starting-2
    Learnt bodyParser.
    Filetering of get and post reuqest. Routing

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("Middle ware working");
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next)=>{
-    console.log("Middleware 2 working");
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Item Name"><input type="text" name="number" placeholder="Item Quantity"><button type="submit">Submit</button></form>')
+
+});
+
+app.post('/product',(req, res)=>{
+    console.log(req.body);
+    res.redirect('/add-product');
+});
+
+app.use('/',(req, res, next)=>{
+    // console.log("Middleware 2 working");
     res.send('<h1>This is Express Document</h1>')
 
-})
+});
 
 // const server = http.createServer(app);
 // server.listen(4000);
